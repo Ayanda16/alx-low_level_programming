bf5709caa86019f3d72aa480efedbf79af99650e
Ayanda16 <ntselea16@gmail.com>
2022-12-10 07:58:11 -0800

    print out a qoute to the standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
-#include <stdlib.h>
-
+#include <unistd.h>
+/**
+ * main - Prints out exatly the quote given
+ *
+ * Return: (1) (success)
+ */
 int main(void)
 {
-	fwrite(STDOUT_FILENO, "and that piece of art is useful\" -Dora Korpa, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful -Dora Korpa, 2015-10-19\n", 59);
 	return (1);
 }
