0b6cdae8a82f281e6664addce2db8b4211e90f41
Ayanda16 <ntselea16@gmail.com>
2023-01-12 04:19:01 -0800

    string_concatenate

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -19,6 +19,12 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	if (s1)
 	{
+		while (s1[s1_len])
+			++s1_len;
+	}
+
+	if (s2)
+	{
 		while (s2_len < n && s2[s2_len])
 			++s2_len;
 	}
