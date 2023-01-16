f2e7d4975635024d0b1f9fcf7f37bd310e25b0e8
Ayanda16 <ntselea16@gmail.com>
2023-01-16 09:52:32 -0800

    defining header files

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,10 @@
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
+
+/**
+ * Description - header file defining a macro named SIZE
+ */
+
+#define SIZE 1024
+
+#endif
---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,10 @@
+#ifndef PI_H
+#define PI_H
+
+/**
+ * Description - header file defining a macro named PI
+ */
+
+#define PI 3.14159265359
+
+#endif
