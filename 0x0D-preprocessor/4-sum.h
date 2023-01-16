a6b7b7cf45fa11075bc3cdc5c4c14d153ba5afed
Ayanda16 <ntselea16@gmail.com>
2023-01-16 10:16:58 -0800

    sum macro

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,13 @@
+#ifndef SUM_H
+#define SUM_H
+
+/**
+ * Descripton: header file for function-like macro SUM(x, y)
+ *  that computes the sum of the numbers x and y
+ *  @x: number 1 to compute the sum of
+ *  @y: number 2 to compute the sum of
+ */
+
+#define SUM(x, y) ((x) + (y))
+
+#endif
