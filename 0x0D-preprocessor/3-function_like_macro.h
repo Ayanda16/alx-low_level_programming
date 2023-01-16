5f95a2b22a62c0181551448526df06d8c12d9204
Ayanda16 <ntselea16@gmail.com>
2023-01-16 10:05:56 -0800

    abs fuction

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,9 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+
+/**
+ * Description - fuction like macro ABS(x) that computes the abs value of x
+ */
+#define ABS(x) ((x) < 0 ? (-x) : (x))
+
+#endif
