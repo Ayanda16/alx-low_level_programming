cf93c5b0e51ef9fc10046ea46d9300a17c84a39e
Ayanda16 <ntselea16@gmail.com>
2023-01-16 10:08:06 -0800

    abs fuction

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -3,7 +3,9 @@
 
 /**
  * Description - fuction like macro ABS(x) that computes the abs value of x
+ *@x: value to compute the absolite value of
  */
+
 #define ABS(x) ((x) < 0 ? (-x) : (x))
 
 #endif
