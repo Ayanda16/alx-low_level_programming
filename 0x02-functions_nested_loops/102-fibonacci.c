1f98cc311697ee97acabc88ac3d2e83a8c707b7c
Ayanda16 <ntselea16@gmail.com>
2023-01-22 06:17:38 -0800

    fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+
+/**
+ * main - prints the first 50 Fibonacci numbers
+ * @t1: term 1
+ * @t2: term 2
+ * @res: result of adding t1 and t2
+ * Return: 0
+ */
+int main(void)
+{
+	int t1 = 1, t2 = 2;
+        int res = t2 + t2;
+	int n = 3;
+
+	printf("%d, %d, ", t1, t2);
+
+	while (res <= 50)
+	{
+		if (n == 0)
+		{
+		printf("%d \n", res);
+		}
+		else
+		{
+			printf("%d, ", res);
+		}
+		t1 = t2;
+		t2 = res;
+
+		res = t1 + t2;
+		n++;
+	}
+	return (0);
+}
