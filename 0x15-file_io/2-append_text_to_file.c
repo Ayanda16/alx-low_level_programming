a1154e2f630456b616347c682ac44435046ef79a
Ayanda16 <ntselea16@gmail.com>
2023-02-10 08:18:17 -0800

    append

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -14,23 +14,24 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd, err, len;
+	int fd;
+	int i;
 
-	if (!filename)
+	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_RDWR | O_APPEND);
-	if (fd < 0)
-		return (-1);
-	if (!text_content)
-	{
-		close(fd);
+	if (text_content == NULL)
 		return (1);
-	}
-	while (*(text_content + len))
-		len++;
-	err = write(fd, text_content, len);
-	close(fd);
-	if (err < 0)
+	fd = open(filename, O_APPEND | O_WRONLY);
+	if (fd == -1)
 		return (-1);
+	for (i = 0; text_content[i] != '\0'; i++)
+	{
+		if (write(fd, &text_content[i], 1) == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+	close(fd);
 	return (1);
 }
