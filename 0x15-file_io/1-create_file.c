c91941abbd7519342caea061c316cac99d62fd3c
Ayanda16 <ntselea16@gmail.com>
2023-02-10 07:58:37 -0800

    create file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+#include <fcntl.h>
+#include <unistd.h>
+
+/**
+ * filename is the name of the file to create
+ * text_content is a NULL terminated string to write to the file
+ * Returns: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, err, len;
+
+	if (filename == NULL)
+		return (-1);
+	if (text_content == NULL)
+		text_content = "";
+
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	if (fd < 0)
+		return (-1);
+
+	while (text_content && *(text_content + len))
+		len++;
+	err = write(fd, text_content, len);
+	close(fd);
+	if (err < 0)
+		return (-1);
+	return (1);
+}
