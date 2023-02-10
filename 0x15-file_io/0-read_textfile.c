ffbd47c014a9153238fae0527ce69f8d409e7032
Ayanda16 <ntselea16@gmail.com>
2023-02-10 07:17:57 -0800

    read text file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,23 +9,37 @@
  * @letters: number of letters to read and print
  *
  * Return: actual number of letters or zero if file cannot be open or read
- * if filename is NULL return 0
+ * if filename is NULL return 0.
  */
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	FILE *file;
-	char c;
-	file= fopen("Requiescat", "RDONLY");
+	int fd = -1, err = 0, len = 0;
+	char *temp_b;
 
-	if (file == NULL)
+	if (!filename)
 		return (0);
-
-	while (!feof(file))
+	temp_b = malloc(sizeof(char) * letters);
+	if (!temp_b)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd < 0)
 	{
-		c = fgetc(file);
-		printf("%c", c);
+		free(temp_b);
+		return (0);
 	}
-	fclose(file);
-	return (0);
+	err = read(fd, temp_b, letters);
+	if (err < 0)
+	{
+		free(temp_b);
+		return (0);
+	}
+	while (temp_b && temp_b[len])
+		len++;
+	err = write(STDOUT_FILENO, temp_b, len);
+	free(temp_b);
+	close(fd);
+	if (err < 0)
+		return (0);
+	return ((ssize_t)err);
 }
