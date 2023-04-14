e3ee2f84c7596bbe0dc87e0bae0231d9b93d31cf
Ayanda16 <ntselea16@gmail.com>
2023-04-14 12:13:03 -0700

    hash table set

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,30 @@
+#include"hash_tables.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * hash_table_t *hash_table_create - creates a hash table
+ * @size: size of the array
+ *
+ * Return: pointer to the newly created hash table
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *htable;
+	unsigned long int i;
+
+	htable = malloc(sizeof(hash_table_t));
+	if (htable == NULL)
+		return (NULL);
+
+	htable->size = size;
+	htable->array = malloc(sizeof(hash_node_t *) * size);
+	if (htable->array == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		htable->array[i] = NULL;
+
+	return (htable);
+}
---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "hash_tables.h"
+
+/**
+ * key_index - returns the index of a key
+ * @key: The key to get an index of
+ * @size: size of the array of the hash table
+ *
+ * Return: the index at which the key/value pair should be stored
+ */
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	return (hash_djb2(key) % size);
+}
