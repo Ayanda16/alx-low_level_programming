e456225b33353e98766ebc7f0140801de70ba061
Ayanda16 <ntselea16@gmail.con>
2023-06-09 22:08:27 +0200

    key index

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,27 @@
+#include"hash_tables.h"
+
+/**
+ * hash_table_creates - creates a new hash table
+ * @size: size of the array
+ *
+ * Return: a pointer to a newly created hash table
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *ht;
+	unsigned long int i;
+
+	table = malloc(sizeof(hash_table_t));
+	if (table == NULL || size == 0)
+		return (NULL);
+
+	table->size = size;
+	table->array = malloc(sizeof(hash_node_t *) * size);
+	if (table->array == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		table->array[i] = NULL;
+
+	return (table);
+}
